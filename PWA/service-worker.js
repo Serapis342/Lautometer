8a53820db2d7557b18a7625fb498522bfe67dd5c
serapis <MyPrivateSpam@protonmail.com>
2025-04-06 00:50:40 +0200

    PWA Github Pages changes 5.0

--- PWA/service-worker.js
+++ PWA/service-worker.js
@@ -1,19 +1,19 @@
 const CACHE_NAME = 'app-cache-v1';
 const ASSETS = [
-    'Lautometer/',
-    'Lautometer/index.html',
-    'Lautometer/script.js',
-    'Lautometer/styles.css',
-    'Lautometer/PWA/offline.html',
-    'Lautometer/icons/pause.svg',
-    'Lautometer/icons/restart.svg',
-    'Lautometer/icons/start.svg',
-    'Lautometer/icons/offline-icon.svg',
-    'Lautometer/icons/microphone.svg',
-    'Lautometer/icons/microphone-512.png',
-    'Lautometer/icons/microphone-256.png',
-    'Lautometer/icons/microphone-128.png',
-    'Lautometer/icons/microphone-64.png',
+    '/',
+    '/index.html',
+    '/script.js',
+    '/styles.css',
+    '/PWA/offline.html',
+    '/icons/pause.svg',
+    '/icons/restart.svg',
+    '/icons/start.svg',
+    'PWA/offline-icon.svg',
+    'PWA/microphone.svg',
+    'PWA/microphone-512.png',
+    'PWA/microphone-256.png',
+    'PWA/microphone-128.png',
+    'PWA/microphone-64.png',
 ];
 
 // Service Worker installieren und statische Assets cachen
@@ -58,9 +58,9 @@ self.addEventListener('fetch', event => {
             });
         }).catch(() => {
             if (event.request.destination === 'image') {
-                return caches.match('Lautometer/icons/offline-icon.svg');
+                return caches.match('/icons/offline-icon.svg');
             } else {
-                return caches.match('Lautometer/PWA/offline.html');
+                return caches.match('PWA/offline.html');
             }
         })
     );
